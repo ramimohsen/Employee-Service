c769e767b0639c67b1828f282e75d4f9b9f34ff9
ramy.othman <ramy.othman@d360.com>
2024-10-14 04:01:20 +0300

    feat(Employee-Service): Add swagger doc

--- src/main/java/org/employeeservice/controller/EmployeeController.java
+++ src/main/java/org/employeeservice/controller/EmployeeController.java
@@ -1,6 +1,10 @@
 package org.employeeservice.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +13,7 @@ import org.employeeservice.exception.custom.EmployeeAlreadyExistException;
 import org.employeeservice.exception.custom.EmployeeHasSubordinatesException;
 import org.employeeservice.exception.custom.InvalidSupervisorAssignmentException;
 import org.employeeservice.exception.custom.ResourceNotFoundException;
+import org.employeeservice.exception.response.ErrorDetails;
 import org.employeeservice.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,16 +28,27 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    @Operation(summary = "Get employee by ID")
+    @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = EmployeeResponse.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorDetails.class), mediaType = "application/json")})
     @GetMapping("/{employeeId}")
     public EmployeeResponse getEmployeeById(@PathVariable("employeeId") Long employeeId) throws ResourceNotFoundException {
         return this.employeeService.getEmployeeById(employeeId);
     }
 
+    @Operation(summary = "Create a new employee")
+    @ApiResponse(responseCode = "201", content = {@Content(schema = @Schema(implementation = EmployeeResponse.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "409", content = {@Content(schema = @Schema(implementation = ErrorDetails.class), mediaType = "application/json")})
     @PostMapping
     public EmployeeResponse createEmployee(@RequestBody @Valid CreateEmployeeRequest createEmployeeRequest) throws EmployeeAlreadyExistException {
         return this.employeeService.createEmployee(createEmployeeRequest);
     }
 
+
+    @Operation(summary = "Delete an employee")
+    @ApiResponse(responseCode = "204", content = @Content)
+    @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorDetails.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "409", content = {@Content(schema = @Schema(implementation = ErrorDetails.class), mediaType = "application/json")})
     @DeleteMapping("/{employeeId}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable("employeeId") Long employeeId) throws ResourceNotFoundException,
             EmployeeHasSubordinatesException {
@@ -40,12 +56,19 @@ public class EmployeeController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Assign supervisor to an employee")
+    @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = AssignEmployeeResponse.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorDetails.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorDetails.class), mediaType = "application/json")})
     @PutMapping("/{employeeId}/assign-supervisor")
     public AssignEmployeeResponse assignEmployeeToSupervisor(@PathVariable("employeeId") Long employeeId,
                                                              @RequestBody @Valid AssignEmployeeRequest assignEmployeeRequest) throws InvalidSupervisorAssignmentException, ResourceNotFoundException {
         return this.employeeService.assignEmployee(employeeId, assignEmployeeRequest.getSupervisorId());
     }
 
+    @Operation(summary = "Update an existing employee")
+    @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = EmployeeResponse.class), mediaType = "application/json")})
+    @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorDetails.class), mediaType = "application/json")})
     @PutMapping("/{employeeId}")
     public EmployeeResponse updateEmployee(@PathVariable Long employeeId,
                                            @RequestBody UpdateEmployeeRequest request) throws ResourceNotFoundException {
